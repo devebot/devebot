9c0f1fcbbcc8aea628c3506a5eed9dd9d2dc14c9
pnhung177 <pnhung177@gmail.com>
2015-06-27 00:13:31 +0700

    improves the logger and index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('devebot is under construction!');
+'use strict';
+
+var configManager = require('./lib/services/config-manager.js');
+var logger = require('./lib/utils/logger.js');
+
+function load(params) {
+  params = params || {};
+
+  logger.trace(' * devebot is starting up with parameters: %s', JSON.stringify(params, null, 2));
+  
+  var appRootPath = params.appRootPath;
+  var config = configManager(appRootPath + '/config');
+}
+
+module.exports = load;
