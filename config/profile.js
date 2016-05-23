628940e66e802d39050e85c23f8d3bcea8ad93ad
Pham Ngoc Hung <pnhung177@acegik.net>
2016-05-23 20:53:37 +0700

    Websocket connection between server/client over SSL

--- config/profile.js
+++ config/profile.js
@@ -1,1 +1,11 @@
-module.exports = {};
+module.exports = {
+  devebot: {
+    host: '0.0.0.0',
+    port: '17779',
+    ssl: {
+      enabled: false,
+      key_file: __dirname + '/../data/ssl/example.key'),
+      crt_file: __dirname + '/../data/ssl/example.crt')
+    }
+  }
+};
