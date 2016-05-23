a3bd7e677714a7e3a7c82f43f48c91bf97380f3b
Pham Ngoc Hung <pnhung177@acegik.net>
2016-05-24 01:45:41 +0700

    Authentication for client/server communication

--- config/profile.js
+++ config/profile.js
@@ -2,6 +2,10 @@ module.exports = {
   devebot: {
     host: '0.0.0.0',
     port: '17779',
+    authen: {
+      disabled: false,
+      tokenStoreFile: __dirname + '/../data/tokenstore.json')
+    },
     ssl: {
       enabled: false,
       key_file: __dirname + '/../data/ssl/example.key'),
