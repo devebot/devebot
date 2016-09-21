1ef084253909c03174b90a8e97111ac760b31a6d
pnhung177 <pnhung177@acegik.net>
2016-09-21 09:17:21 +0700

    Fixbug: make jobqueue-master is independent with jobqueue-worker

--- config/sandbox.js
+++ config/sandbox.js
@@ -1,9 +1,18 @@
 module.exports = {
   devebot: {
-    redis: {
-      host: '127.0.0.1',
-      port: 6379,
-      name: 'devebotjq'
+    jobqueue: {
+      enabled: false,
+      default: 'redis',
+      engines: [
+        {
+          name: 'redis',
+          config: {
+            host: '127.0.0.1',
+            port: 6379,
+            name: 'devebotjq'
+          }
+        }
+      ]
     }
   }
 };
