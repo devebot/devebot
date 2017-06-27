e5f805d35932ad2a652223aed469af84dba1d02d
pnhung177 <pnhung177@acegik.net>
2017-06-27 14:37:23 +0700

    Rename 'context' to 'staging'

--- test/lab/lib/plugin2/config/sandbox.js
+++ test/lab/lib/plugin2/config/sandbox.js
@@ -1,1 +1,14 @@
-module.exports = {}
+module.exports = {
+  plugins: {
+    plugin2: {
+      host: "localhost",
+      port: 17702,
+      tdd: {
+        field1: "String 2",
+        field2: 10002,
+        field3: { foo: "foo", bar: "bar", num: 1002 },
+        field4: [ 1, 2, 3, null, "4" ]
+      }
+    }
+  }
+}
