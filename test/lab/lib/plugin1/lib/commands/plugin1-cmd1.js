168b0999323c57ffd7d3efdc4bcff207c71c34e1
pnhung177 <pnhung177@acegik.net>
2017-06-29 14:26:02 +0700

    Rename commands folder 'lib/scripts' -> 'lib/commands'

---
+++ test/lab/lib/plugin1/lib/commands/plugin1-cmd1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Promise = require('bluebird');
+var lodash = require('lodash');
+
+var commandConfig;
+
+var commandObject = {
+  info: {
+  	description: 'Plugin1 - Command1',
+    options: []
+  },
+  handler: function(opts, ctx) {
+    return Promise.resolve([{
+        type: 'json',
+        title: 'Plugin1 - Command1',
+        data: {}
+    }]);
+  }
+};
+
+module.exports = function(params) {
+  commandConfig = params || {};
+  return commandObject;
+};
---
+++ test/lab/lib/plugin2/lib/commands/plugin2-cmd1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Promise = require('bluebird');
+var lodash = require('lodash');
+
+var commandConfig;
+
+var commandObject = {
+  info: {
+  	description: 'Plugin2 - Command1',
+    options: []
+  },
+  handler: function(opts, ctx) {
+    return Promise.resolve([{
+        type: 'json',
+        title: 'Plugin2 - Command1',
+        data: {}
+    }]);
+  }
+};
+
+module.exports = function(params) {
+  commandConfig = params || {};
+  return commandObject;
+};
