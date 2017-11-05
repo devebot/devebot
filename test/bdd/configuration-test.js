b9b5587a7cf0f197e063d11568d14d40edb57d2f
pnhung177 <pnhung177@acegik.net>
2017-11-05 16:37:00 +0700

    Custom configuration directory using NODE_DEVEBOT_CONFIG_DIR and NODE_DEVEBOT_CONFIG_ENV environment variables

--- test/bdd/configuration-test.js
+++ test/bdd/configuration-test.js
@@ -45,7 +45,7 @@ describe('devebot:configuration', function() {
 						}
 					],
 					"framework": {
-						"version": "0.2.0",
+						"version": "0.2.1",
 						"name": "devebot",
 						"description": "Nodejs Microservice Framework",
 						"homepage": "https://github.com/devebot/devebot#readme",
