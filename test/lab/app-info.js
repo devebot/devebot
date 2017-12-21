406e5011d9457bd911f0311875eab5b6e11d432b
pnhung177 <pnhung177@acegik.net>
2017-12-21 20:29:49 +0700

    Parsing app-info arguments using minimist

--- test/lab/app-info.js
+++ test/lab/app-info.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var main = require('./index').getApp('app-simple');
+var argv = require('minimist')(process.argv.slice(2));
+
+var appName = argv['app'] || argv['a'] || 'app-simple';
+
+var main = require('./index').getApp(appName);
 main.runner.invoke(function(injektor) {
 	console.log('profile config: %s', JSON.stringify(injektor.lookup('profileConfig'), null, 2));
 	return Promise.resolve();
