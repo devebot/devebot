01405698234ff2214a3dbc9fdb6b0fdd3af9a6e5
pnhung177 <pnhung177@acegik.net>
2018-03-20 17:39:21 +0700

    Implement kernel#validateBridgeConfig() function

--- src/kernel.js
+++ src/kernel.js
@@ -44,6 +44,20 @@ function Kernel(params) {
     injektor.defineService(serviceName, constructor, chores.injektorContext);
   });
 
+  var schemaValidator = injektor.lookup('schemaValidator', chores.injektorContext);
+  var result = [];
+
+  var configObject = {
+    profile: lodash.get(params, ['profile', 'mixture'], {}),
+    sandbox: lodash.get(params, ['sandbox', 'mixture'], {})
+  }
+
+  // validate bridge's configures
+  var bridgeLoader = injektor.lookup('bridgeLoader', chores.injektorContext);
+  var bridgeConfig = lodash.get(params, ['sandbox', 'mixture', 'bridges'], {});
+  validateBridgeConfig({LX, LT, bridgeLoader, schemaValidator}, bridgeConfig, result);
+
+  // validate plugin's configures
   var schemaMap = {};
   var pluginLoader = injektor.lookup('pluginLoader', chores.injektorContext);
   pluginLoader.loadSchemas(schemaMap);
@@ -57,7 +71,7 @@ function Kernel(params) {
   }));
 
   var SELECTED_FIELDS = [ 'crateScope', 'schema', 'extension' ];
-  var extractConfigSchema = function(schemaMap) {
+  var extractPluginSchema = function(schemaMap) {
     var configSchema = { profile: {}, sandbox: {} };
     lodash.forOwn(schemaMap, function(ref, key) {
       var def = ref && ref.default || {};
@@ -72,12 +86,7 @@ function Kernel(params) {
     });
     return configSchema;
   }
-  var configSchema = extractConfigSchema(schemaMap);
-
-  var configObject = {
-    profile: lodash.get(params, ['profile', 'mixture'], {}),
-    sandbox: lodash.get(params, ['sandbox', 'mixture'], {})
-  }
+  var configSchema = extractPluginSchema(schemaMap);
 
   LX.has('silly') && LX.log('silly', LT.add({
     configObject: configObject,
@@ -86,9 +95,6 @@ function Kernel(params) {
     tags: [ blockRef, 'config-schema-synchronizing' ],
     text: ' - Synchronize the structure of configuration data and schemas'
   }));
-
-  var schemaValidator = injektor.lookup('schemaValidator', chores.injektorContext);
-  var result = [];
 
   var sandboxObject = configObject.sandbox || {};
   var sandboxSchema = configSchema.sandbox || {};
@@ -105,7 +111,7 @@ function Kernel(params) {
     return output;
   }
 
-  var validateCrateConfig = function(result, crateConfig, crateSchema, crateName) {
+  var validatePluginConfig = function(result, crateConfig, crateSchema, crateName) {
     if (crateSchema && crateSchema.schema) {
       var r = schemaValidator.validate(crateConfig, crateSchema.schema);
       result.push(customizeResult(r, crateConfig, crateSchema, crateName));
@@ -122,17 +128,18 @@ function Kernel(params) {
   }
 
   if (sandboxObject.application) {
-    validateCrateConfig(result, sandboxObject.application, sandboxSchema.application, 'application');
+    validatePluginConfig(result, sandboxObject.application, sandboxSchema.application, 'application');
   }
 
   if (sandboxObject.plugins) {
     lodash.forOwn(sandboxObject.plugins, function(pluginConfig, pluginName) {
       if (lodash.isObject(sandboxSchema.plugins)) {
-        validateCrateConfig(result, pluginConfig, sandboxSchema.plugins[pluginName], pluginName);
+        validatePluginConfig(result, pluginConfig, sandboxSchema.plugins[pluginName], pluginName);
       }
     });
   }
 
+  // summarize validating result
   LX.has('silly') && LX.log('silly', LT.add({
     validatingResult: result
   }).toMessage({
@@ -155,3 +162,49 @@ function Kernel(params) {
 }
 
 module.exports = Kernel;
+
+let validateBridgeConfig = function(ctx, bridgeConfig, result) {
+  let { LX, LT, bridgeLoader, schemaValidator } = ctx;
+  bridgeConfig = bridgeConfig || {};
+  result = result || [];
+
+  let bridgeMetadata = {};
+  bridgeLoader.loadMetadata(bridgeMetadata);
+
+  var customizeResult = function(result, bridgeCode, pluginName, dialectName) {
+    var output = {};
+    output.stage = 'bridge/schema';
+    output.name = pluginName + '/' + bridgeCode + '#' + dialectName;
+    output.type = 'bridge';
+    output.hasError = result.ok !== true;
+    if (!result.ok && result.errors) {
+      output.stack = JSON.stringify(result.errors, null, 2);
+    }
+    return output;
+  }
+
+  var validateDialects = function(metadata, mapping) {
+    LX.has('silly') && LX.log('silly', LT.add({
+      metadata: metadata,
+      mapping: mapping
+    }).toMessage({
+      text: ' - bridge metadata:\n${metadata}\n${mapping}'
+    }));
+    for(var bridgeCode in mapping) {
+      var bridgeMap = mapping[bridgeCode] || {};
+      var _schema = lodash.get(metadata, [bridgeCode, 'metadata', 'schema'], null);
+      if (lodash.isNull(_schema)) continue;
+      for(var pluginName in bridgeMap) {
+        var pluginMap = bridgeMap[pluginName] || {};
+        for(var dialectName in pluginMap) {
+          var dialectConfig = pluginMap[dialectName] || {};
+          var r = schemaValidator.validate(dialectConfig, _schema);
+          result.push(customizeResult(r, bridgeCode, pluginName, dialectName));
+        }
+      }
+    }
+    return result;
+  }
+
+  return validateDialects(bridgeMetadata, bridgeConfig);
+}
